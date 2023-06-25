f6a3368eb618d8eecc3223ee624d60e3ff063d91
Diogo Martins <me.martinsfe@gmail.com>
2023-06-25 17:21:30 -0300

    feat: create functionality to show games filtered by title

--- src/pages/GameList.tsx
+++ src/pages/GameList.tsx
@@ -1,3 +1,48 @@
+import { GameItem } from '../components/GameItem';
+import { useFetch } from '../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import { IDataItem } from '../interfaces/IDataItem';
+
 export function GameList() {
-  return 'GAMELIST';
+  const endpoint = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data';
+  const email = 'meu@email.com.br';
+
+  const [loading, data] = useFetch<IDataItem[]>(endpoint, email);
+  const { str } = useParams();
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  const filteredGames = data.filter((el) =>
+    el.title.toLowerCase().includes(str?.toLowerCase() as string)
+  );
+
+  if (filteredGames.length === 1) {
+    console.log('Somente um titulo encontrado');
+  }
+
+  return (
+    <>
+      <div>
+        <main>
+          <section>
+            <div>
+              <p>
+                Showing results for: <span>{str}</span>
+              </p>
+            </div>
+          </section>
+
+          <section>
+            <div>
+              {filteredGames.map((item) => (
+                <GameItem game={item} key={crypto.randomUUID()} />
+              ))}
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
+  );
 }
