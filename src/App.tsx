9be114356910fda9ecdd81b79a9170b223f65be2
Diogo Martins <me.martinsfe@gmail.com>
2023-06-25 07:23:46 -0300

    feat: create page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { GameList } from './pages/GameList';
+import { Game } from './pages/Game';
+import { NotFound } from './pages/NotFound';
+
 export function App() {
-  return <></>;
+  return (
+    <div>
+      <header>Header</header>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="games">
+          <Route path="search/:str" element={<GameList />}></Route>
+          <Route path=":id" element={<Game />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+
+      <footer>Footer</footer>
+    </div>
+  );
 }
