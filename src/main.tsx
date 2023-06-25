d8bd9a82455c46f5551a046881c08f3aa2c1a8e1
Diogo Martins <me.martinsfe@gmail.com>
2023-06-25 14:29:05 -0300

    feat: add context provider to the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AppProvider } from './context/AppProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
