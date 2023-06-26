5afb1c8a2bb82e131ba83bf78791596e54b524cb
Diogo Martins <me.martinsfe@gmail.com>
2023-06-25 21:08:10 -0300

    feat: create functionality to filter games by genre

--- src/pages/GameList.tsx
+++ src/pages/GameList.tsx
@@ -2,12 +2,14 @@ import { GameItem } from '../components/GameItem';
 import { useFetch } from '../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import { IDataItem } from '../interfaces/IDataItem';
+import { useState } from 'react';
 
 export function GameList() {
   const endpoint = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data';
   const email = 'meu@email.com.br';
 
   const [loading, data, error] = useFetch<IDataItem[]>(endpoint, email);
+  const [filterByGenre, setFilterByGenre] = useState('');
   const { str } = useParams();
 
   if (error) {
@@ -26,6 +28,16 @@ export function GameList() {
     console.log('Somente um titulo encontrado');
   }
 
+  const genres = Array.from(new Set(filteredGames.map((item) => item.genre)));
+
+  function handleChange(e: { target: HTMLSelectElement }): void {
+    if (e.target.value === 'All') {
+      setFilterByGenre('');
+    } else {
+      setFilterByGenre(e.target.value);
+    }
+  }
+
   return (
     <>
       <div>
@@ -35,14 +47,32 @@ export function GameList() {
               <p>
                 Showing results for: <span>{str}</span>
               </p>
+              <label htmlFor="selectGenre">
+                Filter by genre
+                <select
+                  name="selectGenre"
+                  id="selectGenre"
+                  value={filterByGenre}
+                  onChange={(e) => handleChange(e)}
+                >
+                  <option value="All">All</option>
+                  {genres.map((genre) => (
+                    <option key={crypto.randomUUID()} value={genre}>
+                      {genre}
+                    </option>
+                  ))}
+                </select>
+              </label>
             </div>
           </section>
 
           <section>
             <div>
-              {filteredGames.map((item) => (
-                <GameItem game={item} key={crypto.randomUUID()} />
-              ))}
+              {filteredGames
+                .filter(({ genre }) => genre.includes(filterByGenre))
+                .map((item) => (
+                  <GameItem game={item} key={crypto.randomUUID()} />
+                ))}
             </div>
           </section>
         </main>
