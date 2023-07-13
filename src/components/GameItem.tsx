296a05eeaafe0bc8ecfc514ae1f14b4aaddecf12
Diogo Martins <me.martinsfe@gmail.com>
2023-07-13 07:02:17 -0300

    feat: starting part2, included rate and fav for each game

--- src/components/GameItem.tsx
+++ src/components/GameItem.tsx
@@ -1,24 +1,67 @@
-import { IGameProp } from '../interfaces/IGameProp';
-import style from './_gameitem.module.scss';
+import { Link } from 'react-router-dom'
+import { IGameProp } from '../interfaces/IGameProp'
+import style from './_gameitem.module.scss'
+import { ArrowSquareOut, Heart, Star } from '@phosphor-icons/react'
 
 export function GameItem(props: IGameProp) {
-  const { game } = props;
+  const { game } = props
+  const num = 3
 
   return (
     <div className={style.card}>
       <img src={game.thumbnail} alt={game.title} className={style.card__img} />
+      <button className={style.favorite}>
+        <Heart size={24} weight="fill" />
+      </button>
       <div className={style.card__content}>
-        <div className={style.content__title}>
-          <p>{game.title}</p>
+        <p className={style.content__title}>{game.title}</p>
+        {}
+        <div className={style.rating_container}>
+          <input
+            type="radio"
+            name={`rate-${game.id}`}
+            id={`rate-5-${game.id}`}
+            onChange={() => console.log('rate-5')}
+          />
+          <label htmlFor={`rate-5-${game.id}`}>★</label>
+          <input
+            type="radio"
+            name={`rate-${game.id}`}
+            id={`rate-4-${game.id}`}
+            onChange={() => console.log('rate-4')}
+          />
+          <label htmlFor={`rate-4-${game.id}`}>★</label>
+          <input
+            type="radio"
+            name={`rate-${game.id}`}
+            id={`rate-3-${game.id}`}
+            onChange={() => console.log('rate-3')}
+          />
+          <label htmlFor={`rate-3-${game.id}`}>★</label>
+          <input
+            type="radio"
+            name={`rate-${game.id}`}
+            id={`rate-2-${game.id}`}
+            onChange={() => console.log('rate-2')}
+          />
+          <label htmlFor={`rate-2-${game.id}`}>★</label>
+          <input
+            type="radio"
+            name={`rate-${game.id}`}
+            id={`rate-1-${game.id}`}
+            onChange={() => console.log('rate-1')}
+          />
+          <label htmlFor={`rate-1-${game.id}`}>★</label>
         </div>
-        <div className={style.content__description}>{game.short_description}</div>
-        <div className={style.content__platforms}>
-          <p>{game.platform}</p>
+        <p className={style.content__description}>{game.short_description}</p>
+        <div>
+          <p className={style.content__genre}>{game.genre}</p>
+          <p className={style.content__platforms}>{game.platform}</p>
         </div>
-        <div className={style.content__genre}>
-          <p>{game.genre}</p>
-        </div>
+        <Link to={game.game_url} className={style.link}>
+          Visit Webpage <ArrowSquareOut size={20} weight="bold" />
+        </Link>
       </div>
     </div>
-  );
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,58 @@
-import { useState } from 'react';
-import { useFetch } from '../hooks/useFetch';
-import { GameItem } from '../components/GameItem';
-import { Link, useNavigate } from 'react-router-dom';
-import { IDataItem } from '../interfaces/IDataItem';
-import styles from './_home.module.scss';
+import { useState } from 'react'
+import { useFetch } from '../hooks/useFetch'
+import { GameItem } from '../components/GameItem'
+import { useNavigate } from 'react-router-dom'
+import { IDataItem } from '../interfaces/IDataItem'
+import styles from './_home.module.scss'
+import { Loading } from '../components/Loading'
 
 export function Home() {
-  const endpoint = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data';
-  const email = 'meu@email.com.br';
+  const endpoint = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data'
+  const email = 'meu@email.com.br'
 
-  const [loading, data, error] = useFetch<IDataItem[]>(endpoint, email);
-  const [searchContent, setSearchContent] = useState('');
-  const navigate = useNavigate();
+  const [loading, data, error] = useFetch<IDataItem[]>(endpoint, email)
+  const [searchContent, setSearchContent] = useState('')
+  const navigate = useNavigate()
 
   if (error) {
-    return error;
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          fontSize: '1.5rem',
+          maxWidth: '42ch',
+          fontWeight: 900,
+        }}
+      >
+        {error}
+      </div>
+    )
   }
 
   if (loading) {
-    return 'Loading...';
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <Loading />
+      </div>
+    )
   }
 
   function handleChange(e: { target: HTMLInputElement }): void {
-    setSearchContent(e.target.value);
+    setSearchContent(e.target.value)
   }
 
   function handleClick(): void {
-    navigate(`/games/search/${searchContent}`);
+    navigate(`/games/search/${searchContent}`)
   }
 
   return (
@@ -35,7 +61,7 @@ export function Home() {
         <main className={styles.home__container}>
           <section>
             <div className={styles.hero}>
-              <h1>Find your new free to play game!</h1>
+              <h1>Discover new awesome free games</h1>
               <div className={styles.searchContainer}>
                 <input
                   type="text"
@@ -54,14 +80,12 @@ export function Home() {
             <h2>Check out this awesome games!</h2>
             <div className={styles.gamelist}>
               {data?.map((item) => (
-                <Link to={item.game_url} key={crypto.randomUUID()}>
-                  <GameItem game={item} />
-                </Link>
+                <GameItem key={crypto.randomUUID()} game={item} />
               ))}
             </div>
           </section>
         </main>
       </div>
     </>
-  );
+  )
 }
